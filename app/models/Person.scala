f992136a1683304331ab1f2ad64d6ae1a05f7e3e
Marcos <marcos.celio@aurea.com>
2017-08-21 22:21:39 -0300

    fix controller

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json._
 
-case class Person(id: Long, name: String, age: Int)
+case class Person(id: Long, name: String, lastname: String, age: Int, email: String, passport: String)
 
 object Person {
   
