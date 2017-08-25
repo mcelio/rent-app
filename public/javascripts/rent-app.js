82b21187d5aa8503f48ac5717e36a05609593f37
Marcos <marcos.celio@aurea.com>
2017-08-24 22:57:55 -0300

    Fix alerts

--- public/javascripts/rent-app.js
+++ public/javascripts/rent-app.js
@@ -16,6 +16,47 @@ app.controller('PersonList', function($http, $scope) {
      .then(function(response) {
          $scope.personList = response.data;
      });
+
+     $scope.success = false;
+     $scope.fail = false;
+     $scope.name;
+     $scope.lastname;
+     $scope.age;
+     $scope.email;
+     $scope.passport;
+
+    $scope.clearForm = function(){
+        $scope.name = '';
+        $scope.lastname = '';
+        $scope.age = '';
+        $scope.email = '';
+        $scope.passport = '';
+    }
+
+     $scope.closeFail = function(){
+        $scope.fail = false;
+     }
+
+     $scope.closeSuccess = function(){
+         $scope.success = false;
+      }
+
+     $scope.handleClick = function(){
+        var jsonData = JSON.stringify({'name':$scope.name, 'lastname':$scope.lastname,'age':$scope.age,'email':$scope.email,'passport':$scope.passport});
+        $http.post('/createPerson', jsonData).then(function success(response) {
+              var data = response.data;
+              $http.get("/persons")
+                   .then(function(response) {
+                       $scope.personList = response.data;
+                   });
+              $scope.success = true;
+              $scope.clearForm();
+            },
+            function error(response) {
+              var data = response.data;
+              $scope.fail = true;
+           });
+     };
 });
 
 app.controller('ContractList', function($http, $scope) {
@@ -23,4 +64,23 @@ app.controller('ContractList', function($http, $scope) {
      .then(function(response) {
          $scope.contractList = response.data;
      });
+});
+
+
+app.factory("flash", function($rootScope) {
+  var queue = [];
+  var currentMessage = "";
+
+  $rootScope.$on("$routeChangeSuccess", function() {
+    currentMessage = queue.shift() || "";
+  });
+
+  return {
+    setMessage: function(message) {
+      queue.push(message);
+    },
+    getMessage: function() {
+      return currentMessage;
+    }
+  };
 });
