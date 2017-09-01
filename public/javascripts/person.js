ac8104b64a0b43dc453f3908f38329a3bc31db1d
Marcos <marcos.celio@aurea.com>
2017-09-01 14:22:57 -0300

    Adjust to rest producer/consumer

--- public/javascripts/person.js
+++ public/javascripts/person.js
@@ -33,6 +33,19 @@ app.controller('PersonList', function($http, $scope) {
         $scope.passport = '';
     }
 
+    $scope.clearMessages = function(){
+        $scope.success = false;
+        $scope.fail = false;
+    }
+
+    $scope.clearForm = function(){
+            $scope.name = '';
+            $scope.lastname = '';
+            $scope.age = '';
+            $scope.email = '';
+            $scope.passport = '';
+        }
+
      $scope.closeFail = function(){
         $scope.fail = false;
      }
@@ -41,7 +54,24 @@ app.controller('PersonList', function($http, $scope) {
          $scope.success = false;
       }
 
-     $scope.handleClick = function(){
+     $scope.removePerson = function(person){
+        $scope.clearMessages();
+        $http.delete('/person/' + person.id).then(
+                function(response){
+                  // success callback
+                  $http.get("/persons").then(function(response) {
+                         $scope.personList = response.data;
+                     });
+                },
+                function(response){
+                  // failure call back
+                }
+             );
+        $scope.success = true;
+     }
+
+     $scope.insertPerson = function(){
+        $scope.clearMessages();
         var jsonData = JSON.stringify({'name':$scope.name, 'lastname':$scope.lastname,'age':$scope.age,'email':$scope.email,'passport':$scope.passport});
         $http.post('/createPerson', jsonData).then(function success(response) {
               var data = response.data;
