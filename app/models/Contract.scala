94af5a44c47a68621bee6c033bb088d8ff9eae89
Marcos <marcos.celio@aurea.com>
2017-09-02 12:01:48 -0300

    Adding releationship to contract entity

--- app/models/Contract.scala
+++ app/models/Contract.scala
@@ -4,7 +4,8 @@ import java.util.Date
 
 import play.api.libs.json._
 
-case class Contract(id: Long, personId: Long, beginDate: String, endDate: String)
+case class Contract(id: Long, personId: Long, beginDate: String, endDate: String, numberAdvances: Int,
+                    numberDeposits: Int, rentAmount: Double, depositAmount: Double, notes: String, removed: Boolean)
 
 object Contract {
 
