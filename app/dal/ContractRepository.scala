fc737d64c70d447a83246f75815c7ec85ac1fd7b
Marcos <marcos.celio@aurea.com>
2017-09-02 12:01:45 -0300

    Adding releationship to contract entity

--- app/dal/ContractRepository.scala
+++ app/dal/ContractRepository.scala
@@ -1,12 +1,12 @@
 package dal
 
-import javax.inject.{ Inject, Singleton }
+import javax.inject.{Inject, Singleton}
+
 import play.api.db.slick.DatabaseConfigProvider
 import slick.jdbc.JdbcProfile
+import models.{Contract, Person}
 
-import models.Contract
-
-import scala.concurrent.{ Future, ExecutionContext }
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * A repository for Contract.
@@ -40,6 +40,24 @@ class ContractRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(im
     /** The beginDate column */
     def endDate = column[String]("endDate")
 
+    /** The numberAdvances column */
+    def numberAdvances = column[Int]("numberAdvances")
+
+    /** The numberDeposits column */
+    def numberDeposits = column[Int]("numberDeposits")
+
+    /** The rentAmount column */
+    def rentAmount = column[Double]("rentAmount")
+
+    /** The depositAmount column */
+    def depositAmount = column[Double]("depositAmount")
+
+    /** The notes column */
+    def notes = column[String]("notes")
+
+    /** The removed column */
+    def removed = column[Boolean]("removed", O.Default(false))
+
     /**
       * This is the tables default "projection".
       *
@@ -48,7 +66,8 @@ class ContractRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(im
       * In this case, we are simply passing the id, name and page parameters to the Person case classes
       * apply and unapply methods.
       */
-    def * = (id, personId, beginDate, endDate) <> ((Contract.apply _).tupled, Contract.unapply)
+    def * = (id, personId, beginDate, endDate, numberAdvances, numberDeposits, rentAmount, depositAmount, notes,
+      removed) <> ((Contract.apply _).tupled, Contract.unapply)
   }
 
   /**
@@ -62,22 +81,39 @@ class ContractRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(im
     * This is an asynchronous operation, it will return a future of the created person, which can be used to obtain the
     * id for that person.
     */
-  def create(personId: Long, beginDate: String, endDate: String): Future[Contract] = db.run {
+  def create(personId: Long, beginDate: String, endDate: String, numberAdvances: Int, numberDeposits: Int,
+             rentAmount: Double, depositAmount: Double, notes: String, removed: Boolean): Future[Contract] = db.run {
     // We create a projection of just the name and age columns, since we're not inserting a value for the id column
-    (contracts.map(c => (c.personId, c.beginDate, c.endDate))
+    (contracts.map(c => (c.personId, c.beginDate, c.endDate, c.numberAdvances, c.numberDeposits, c.rentAmount,
+      c.depositAmount, c.notes, c.removed))
       // Now define it to return the id, because we want to know what id was generated for the person
       returning contracts.map(_.id)
       // And we define a transformation for the returned value, which combines our original parameters with the
       // returned id
-      into ((contractData, id) => Contract(id, contractData._1, contractData._2, contractData._3))
+      into ((contractData, id) => Contract(id, contractData._1, contractData._2, contractData._3, contractData._4,
+      contractData._5, contractData._6, contractData._7, contractData._8, contractData._9))
       // And finally, insert the person into the database
-      ) += (personId, beginDate, endDate)
+      ) += (personId, beginDate, endDate, numberAdvances, numberDeposits, rentAmount, depositAmount, notes, removed)
   }
 
   /**
     * List all the people in the database.
     */
   def list(): Future[Seq[Contract]] = db.run {
-    contracts.result
+    contracts.filter(!_.removed).result
+  }
+
+  /**
+    * Update contracts
+    * @param id
+    * @return
+    */
+  def delete(id: Long): Future[Int] = {
+    val q = for {c <- contracts if c.id === id} yield c.removed
+    db.run(q.update(true))
+
+    //    contracts.filter(_.id === id).map { c =>
+    //      c.removed
+    //    }.update(true)
   }
 }
