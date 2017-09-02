be43f7898b03912cdd8ac33e4a8b6768c50d8dbf
Marcos <marcos.celio@aurea.com>
2017-09-02 12:01:49 -0300

    Adding releationship to contract entity

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json._
 
-case class Person(id: Long, name: String, lastname: String, age: Int, email: String, passport: String)
+case class Person(id: Long, name: String, lastname: String, age: Int, email: String, passport: String, removed: Boolean)
 
 object Person {
   
