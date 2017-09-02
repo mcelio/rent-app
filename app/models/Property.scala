39a087982cf72984eddfa580dbf70e833e27b20f
Marcos <marcos.celio@aurea.com>
2017-09-02 12:01:50 -0300

    Adding releationship to contract entity

--- app/models/Property.scala
+++ app/models/Property.scala
@@ -1,5 +1,10 @@
 package models
 
-class Property {
+import play.api.libs.json._
 
+case class Property(id: Long, code: String, line1: String, line2: String, line3: String, zipcode: String, rooms: Int, removed: Boolean)
+
+object Property {
+
+  implicit val propertyFormat = Json.format[Property]
 }
