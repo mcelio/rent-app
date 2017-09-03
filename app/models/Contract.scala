a62369822ece8b23a4ffd90e14d98e793c1ec0ec
Marcos <marcos.celio@aurea.com>
2017-09-03 16:53:00 -0300

    Adding security

--- app/models/Contract.scala
+++ app/models/Contract.scala
@@ -4,7 +4,7 @@ import java.util.Date
 
 import play.api.libs.json._
 
-case class Contract(id: Long, personId: Long, beginDate: String, endDate: String, numberAdvances: Int,
+case class Contract(id: Long, propertyId: Long, personId: Long, beginDate: String, endDate: String, numberAdvances: Int,
                     numberDeposits: Int, rentAmount: Double, depositAmount: Double, notes: String, removed: Boolean)
 
 object Contract {
