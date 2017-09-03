42d1ee3379401811f6083f4d73cf8712f68dbefa
Marcos <marcos.celio@aurea.com>
2017-09-03 16:52:58 -0300

    Adding security

--- app/controllers/ContractController.scala
+++ app/controllers/ContractController.scala
@@ -3,7 +3,7 @@ package controllers
 import javax.inject._
 
 import dal._
-import models.{Contract, Person}
+import models.{Contract, Person, Property}
 import play.api.i18n._
 import play.api.libs.json.{JsValue, Json, Writes}
 import play.api.mvc._
@@ -13,21 +13,22 @@ import scala.util.{Failure, Success}
 
 class ContractController @Inject()(repo: ContractRepository,
                                    personRepo: PersonRepository,
+                                   propertyRepo: PropertyRepository,
                                    cc: ControllerComponents
                                 )(implicit ec: ExecutionContext)
   extends AbstractController(cc) with I18nSupport {
 
 
-  implicit val writer = new Writes[(Contract, Seq[Person])] {
-    def writes(t: (Contract, Seq[Person])): JsValue = {
+  implicit val writer = new Writes[(Contract, Seq[Person], Seq[Property])] {
+    def writes(t: (Contract, Seq[Person], Seq[Property])): JsValue = {
       val personName = t._2(0).name
       val passport = t._2(0).passport
-
+      val propertyCode = t._3(0).code
 
       Json.obj("id: " -> t._1.id, "beginDate" -> t._1.beginDate, "endDate" -> t._1.endDate, "notes" -> t._1.notes,
         "rentAmount" -> t._1.rentAmount, "depositAmout" -> t._1.depositAmount,
         "numberAdvances" -> t._1.numberAdvances, "numberDeposits" -> t._1.numberDeposits, "personName" -> personName,
-        "passport" -> passport)
+        "passport" -> passport, "propertyCode" -> propertyCode)
     }
   }
 
@@ -35,12 +36,18 @@ class ContractController @Inject()(repo: ContractRepository,
     * The contract action.
     */
   def contract = Action { implicit request =>
-    Ok(views.html.contract("contract"))
+    val auth = request.session.get("authorization").getOrElse("false").toBoolean
+    if (auth) {
+      Ok(views.html.contract("contract"))
+    } else{
+      Ok(views.html.login())
+    }
   }
 
   def createContract() = Action.async { implicit request =>
     implicit val contractReader = Json.reads[Contract]
     val jsonBody: JsValue = request.body.asJson.get
+    val propertyId = (jsonBody \ "propertyId").as[Long]
     val personId = (jsonBody \ "personId").as[Long]
     val beginDate = (jsonBody \ "beginDate").as[String]
     val endDate = (jsonBody \ "endDate").as[String]
@@ -49,7 +56,7 @@ class ContractController @Inject()(repo: ContractRepository,
     val numberDeposits = (jsonBody \ "numberDeposits").as[String]
     val depositAmount = (jsonBody \ "depositAmount").as[String]
     val notes = (jsonBody \ "notes").as[String]
-    repo.create(personId, beginDate, endDate, numberAdvances.toInt, numberDeposits.toInt, rentAmount.toDouble,
+    repo.create(propertyId, personId, beginDate, endDate, numberAdvances.toInt, numberDeposits.toInt, rentAmount.toDouble,
       depositAmount.toDouble, notes, false) map {personObj =>
       Future {
         Ok(Json.toJson(personObj))
@@ -74,7 +81,7 @@ class ContractController @Inject()(repo: ContractRepository,
   def getContracts = Action.async { implicit request =>
     repo.list().map(contracts =>
       contracts.map(contract => {
-        (contract, personRepo.findById(contract.personId))
+        (contract, personRepo.findById(contract.personId), propertyRepo.findById(contract.propertyId))
       })
     ).map { results =>
       Ok(Json.toJson(results))
