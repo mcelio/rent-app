fcdc2f1002224d5f7ce4b36c86342669d284cbb8
Marcos <marcos.celio@aurea.com>
2017-09-03 16:53:01 -0300

    Adding security

--- app/utils/AuthorizationUtils.scala
+++ app/utils/AuthorizationUtils.scala
@@ -2,4 +2,15 @@ package utils
 
 class AuthorizationUtils {
 
+
+  def authorize(username: String, password: String): Boolean = {
+    if(username == "Fernanda" && password == "Rent@123"){
+        return true;
+    }
+    return false;
+  }
+
+  def isAuth(): Boolean ={
+       true
+  }
 }
